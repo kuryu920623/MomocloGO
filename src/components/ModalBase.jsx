4b34c534a36867ef35719eb52dd7461c6d0df223
sora <sora@sora.com>
2022-01-12 20:37:50 +0900

    モーダル切り出し

---
+++ src/components/ModalBase.jsx
@@ -0,0 +1,82 @@
+import React from 'react';
+import {
+  Modal, StyleSheet, Text, Pressable, View,
+} from 'react-native';
+import { func, bool, elementType } from 'prop-types';
+
+export default function ModalBase(props) {
+  // const [modalVisible, setModalVisible] = useState(false);
+  const { children, onPress, modalVisible } = props;
+
+  return (
+    <View style={styles.centeredView}>
+
+      <Modal
+        animationType="fade"
+        transparent
+        visible={modalVisible}
+      >
+        <Pressable
+          activeOpacity={1}
+          style={[styles.centeredView, styles.modalBackGroundView]}
+          onPress={() => onPress(!modalVisible)}
+        />
+        <View style={styles.modalView}>
+          {children}
+          <Pressable
+            style={[styles.button, styles.buttonClose]}
+            onPress={() => onPress(!modalVisible)}
+          >
+            <Text style={styles.textStyle}>閉じる</Text>
+          </Pressable>
+        </View>
+
+      </Modal>
+    </View>
+  );
+}
+
+ModalBase.propTypes = {
+  children: elementType.isRequired,
+  onPress: func.isRequired,
+  modalVisible: bool,
+};
+
+const styles = StyleSheet.create({
+  centeredView: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  modalBackGroundView: {
+    opacity: 0.7,
+    backgroundColor: 'black',
+  },
+  modalView: {
+    position: 'absolute',
+    top: '20%',
+    height: '60%',
+    width: '80%',
+    alignSelf: 'center',
+    backgroundColor: 'white',
+    borderRadius: 5,
+    padding: 35,
+    alignItems: 'center',
+  },
+  button: {
+    borderRadius: 20,
+    padding: 10,
+    elevation: 2,
+  },
+  buttonOpen: {
+    backgroundColor: '#F194FF',
+  },
+  buttonClose: {
+    backgroundColor: '#2196F3',
+  },
+  textStyle: {
+    color: 'white',
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+});
--- src/screens/MainScreen.jsx
+++ src/screens/MainScreen.jsx
@@ -1,74 +1,57 @@
 import React, { useState } from 'react';
 import {
-  Modal, StyleSheet, Text, Pressable, View, TouchableOpacity, Dimensions,
+  StyleSheet, Text, Pressable, View,
 } from 'react-native';
+
+import ModalBase from '../components/ModalBase';
 
 export default function MainScreen() {
   const [modalVisible, setModalVisible] = useState(false);
 
   return (
-    <View style={styles.centeredView}>
-
-      <Modal
-        animationType="fade"
-        transparent
-        visible={modalVisible}
+    <>
+      <ModalBase
+        onPress={setModalVisible}
+        modalVisible={modalVisible}
       >
+        <Text style={styles.modalText}>Hello World!</Text>
+      </ModalBase>
+      <View style={styles.centeredView}>
         <Pressable
-          activeOpacity={1}
-          style={[styles.centeredView, {opacity: 0.7, backgroundColor: 'black'}]}
-          onPress={() => setModalVisible(!modalVisible)}
-        />
-        <View style={styles.modalView}>
-          <Text style={styles.modalText}>Hello World!</Text>
-          <Pressable
-            style={[styles.button, styles.buttonClose]}
-            onPress={() => setModalVisible(!modalVisible)}
-          >
-            <Text style={styles.textStyle}>Hide Modal</Text>
-          </Pressable>
-        </View>
-      </Modal>
-
-      <Pressable
-        style={[styles.button, styles.buttonOpen]}
-        onPress={() => setModalVisible(true)}
-      >
-        <Text style={styles.textStyle}>Show Modal</Text>
-      </Pressable>
-    </View>
+          style={[styles.button, styles.buttonOpen]}
+          onPress={() => setModalVisible(true)}
+        >
+          <Text style={styles.textStyle}>Show Modal</Text>
+        </Pressable>
+      </View>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   centeredView: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
+    justifyContent: 'center',
+    alignItems: 'center',
   },
   modalBackGroundView: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+    opacity: 0.7,
+    backgroundColor: 'black',
   },
   modalView: {
     position: 'absolute',
+    top: '20%',
+    height: '60%',
+    width: '80%',
     alignSelf: 'center',
-    top: Dimensions.get('window').height / 3,
-    margin: 20,
     backgroundColor: 'white',
-    borderRadius: 20,
+    borderRadius: 5,
     padding: 35,
     alignItems: 'center',
-    shadowColor: '#000',
-    shadowOffset: {
-      width: 0,
-      height: 2,
-    },
-    shadowOpacity: 0.25,
-    shadowRadius: 3.84,
-    elevation: 5,
+  },
+  modalText: {
+    marginBottom: 15,
+    textAlign: 'center',
   },
   button: {
     borderRadius: 20,
@@ -76,18 +59,14 @@ const styles = StyleSheet.create({
     elevation: 2,
   },
   buttonOpen: {
-    backgroundColor: "#F194FF",
+    backgroundColor: '#F194FF',
   },
   buttonClose: {
-    backgroundColor: "#2196F3",
+    backgroundColor: '#2196F3',
   },
   textStyle: {
-    color: "white",
-    fontWeight: "bold",
-    textAlign: "center",
-  },
-  modalText: {
-    marginBottom: 15,
-    textAlign: "center",
+    color: 'white',
+    fontWeight: 'bold',
+    textAlign: 'center',
   },
 });

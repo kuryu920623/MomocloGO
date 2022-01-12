be1adfcfe4dd825f27cc899ddfd4beb2f3ce4863
sora <sora@sora.com>
2022-01-12 18:41:27 +0900

    モーダル実験

--- App.jsx
+++ App.jsx
@@ -6,6 +6,8 @@ import firebase from 'firebase';
 import { firebaseConfig } from './env';
 
 import SignUpScreen from './src/screens/SignUpScreen';
+import LogInScreen from './src/screens/LogInScreen';
+import MainScreen from './src/screens/MainScreen';
 
 const Stack = createStackNavigator();
 
@@ -17,17 +19,19 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator
-        initialRouteName="LogIn"
+        initialRouteName="Main"
         screenOptions={{
           headerStyle: { backgroundColor: '#467FD3' },
           headerTitleStyle: { color: '#FFFFFF' },
-          headerTitle: 'Memo App',
+          headerTitle: 'ももクロGO',
           headerTintColor: '#FFFFFF',
           headerBackTitle: 'Back',
           cardStyleInterpolator: CardStyleInterpolators.forHorizontalIOS,
         }}
       >
         <Stack.Screen name="SignUp" component={SignUpScreen} />
+        <Stack.Screen name="LogIn" component={LogInScreen} />
+        <Stack.Screen name="Main" component={MainScreen} />
       </Stack.Navigator>
 
     </NavigationContainer>
---
+++ src/screens/MainScreen.jsx
@@ -0,0 +1,93 @@
+import React, { useState } from 'react';
+import {
+  Modal, StyleSheet, Text, Pressable, View, TouchableOpacity, Dimensions,
+} from 'react-native';
+
+export default function MainScreen() {
+  const [modalVisible, setModalVisible] = useState(false);
+
+  return (
+    <View style={styles.centeredView}>
+
+      <Modal
+        animationType="fade"
+        transparent
+        visible={modalVisible}
+      >
+        <Pressable
+          activeOpacity={1}
+          style={[styles.centeredView, {opacity: 0.7, backgroundColor: 'black'}]}
+          onPress={() => setModalVisible(!modalVisible)}
+        />
+        <View style={styles.modalView}>
+          <Text style={styles.modalText}>Hello World!</Text>
+          <Pressable
+            style={[styles.button, styles.buttonClose]}
+            onPress={() => setModalVisible(!modalVisible)}
+          >
+            <Text style={styles.textStyle}>Hide Modal</Text>
+          </Pressable>
+        </View>
+      </Modal>
+
+      <Pressable
+        style={[styles.button, styles.buttonOpen]}
+        onPress={() => setModalVisible(true)}
+      >
+        <Text style={styles.textStyle}>Show Modal</Text>
+      </Pressable>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  centeredView: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  modalBackGroundView: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+  },
+  modalView: {
+    position: 'absolute',
+    alignSelf: 'center',
+    top: Dimensions.get('window').height / 3,
+    margin: 20,
+    backgroundColor: 'white',
+    borderRadius: 20,
+    padding: 35,
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
+  },
+  button: {
+    borderRadius: 20,
+    padding: 10,
+    elevation: 2,
+  },
+  buttonOpen: {
+    backgroundColor: "#F194FF",
+  },
+  buttonClose: {
+    backgroundColor: "#2196F3",
+  },
+  textStyle: {
+    color: "white",
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+  modalText: {
+    marginBottom: 15,
+    textAlign: "center",
+  },
+});

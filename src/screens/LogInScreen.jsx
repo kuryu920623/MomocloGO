add7c5cbe69bb7b4f8b0f5490ef6b86a703b46ef
sora <sora@sora.com>
2022-03-13 13:32:49 +0900

    ログイン画面、読み込み中表示

--- src/screens/LogInScreen.jsx
+++ src/screens/LogInScreen.jsx
@@ -3,11 +3,10 @@ import {
   View, StyleSheet, TextInput, Text, TouchableOpacity, Alert,
 } from 'react-native';
 import firebase from 'firebase';
-import * as FileSystem from 'expo-file-system';
-import * as SQLite from 'expo-sqlite';
 
+import Loading from '../components/Loading';
 import Button from '../components/Button';
-import { UserContext, transrateErrors } from '../utils/settings';
+import { transrateErrors } from '../utils/settings';
 import RefleshDBandFlagInfomation from '../utils/InitDataBase';
 
 let navigation;
@@ -17,12 +16,15 @@ export default function LogInScreen(props) {
   navigation = props.navigation;
   const [userId, setUserid] = useState('');
   const [password, setPassword] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
+    setIsLoading(false);
     const unsubscribe = firebase.auth().onAuthStateChanged((user) => {
       if (user && !refleshStart) {
         console.log('auto Login', user.email);
         const userid = user.email.replace('@dummy1234321.com', '');
+        setIsLoading(true);
         RefleshDBandFlagInfomation(userid, navigation);
       }
     });
@@ -30,6 +32,7 @@ export default function LogInScreen(props) {
   }, []);
 
   function submit() {
+    setIsLoading(true);
     firebase.auth().signInWithEmailAndPassword(`${userId}@dummy1234321.com`, password)
       .then((userCredential) => {
         refleshStart = true;
@@ -39,6 +42,7 @@ export default function LogInScreen(props) {
         RefleshDBandFlagInfomation(userid, navigation);
       })
       .catch((error) => {
+        setIsLoading(false);
         console.log(error.code, error.message);
         const err = transrateErrors(error.code);
         Alert.alert(err.title, err.description);
@@ -46,48 +50,52 @@ export default function LogInScreen(props) {
   }
 
   return (
-    <View style={styles.container}>
-      <View style={styles.inner}>
-        <Text style={styles.title}>ログイン</Text>
+    <>
+      <Loading isLoading={isLoading} />
+      <View style={styles.container}>
+        <View style={styles.inner}>
+          <Text style={styles.title}>ログイン</Text>
 
-        <View>
-          <Text style={styles.label}>ユーザーID</Text>
-          <TextInput
-            value={userId}
-            style={styles.input}
-            onChangeText={(text) => { setUserid(text); }}
-            autoCapitalize="none"
-            placeholder="userID"
-            textContentType="emailAddress"
-          />
+          <View>
+            <Text style={styles.label}>ユーザーID</Text>
+            <TextInput
+              value={userId}
+              style={styles.input}
+              onChangeText={(text) => { setUserid(text); }}
+              autoCapitalize="none"
+              autoFocus
+              placeholder="userID"
+              textContentType="emailAddress"
+            />
+          </View>
+
+          <View>
+            <Text style={styles.label}>パスワード</Text>
+            <TextInput
+              value={password}
+              style={styles.input}
+              onChangeText={(text) => { setPassword(text); }}
+              autoCapitalize="none"
+              placeholder="Password"
+              secureTextEntry
+              textContentType="password"
+            />
+          </View>
+
+          <View style={styles.buttonLine}>
+            <Button onPress={() => submit()} style={styles.logInButton} label="ログイン" />
+          </View>
+
+          <View style={styles.signUpLink}>
+            <Text>新規登録は </Text>
+            <TouchableOpacity onPress={() => { navigation.navigate('SignUp'); }}>
+              <Text style={styles.signUpLinkText}>こちら</Text>
+            </TouchableOpacity>
+          </View>
+
         </View>
-
-        <View>
-          <Text style={styles.label}>パスワード</Text>
-          <TextInput
-            value={password}
-            style={styles.input}
-            onChangeText={(text) => { setPassword(text); }}
-            autoCapitalize="none"
-            placeholder="Password"
-            secureTextEntry
-            textContentType="password"
-          />
-        </View>
-
-        <View style={styles.buttonLine}>
-          <Button onPress={() => submit()} style={styles.logInButton} label="ログイン" />
-        </View>
-
-        <View style={styles.signUpLink}>
-          <Text>新規登録は </Text>
-          <TouchableOpacity onPress={() => { navigation.navigate('SignUp'); }}>
-            <Text style={styles.signUpLinkText}>こちら</Text>
-          </TouchableOpacity>
-        </View>
-
       </View>
-    </View>
+    </>
   );
 }
 
